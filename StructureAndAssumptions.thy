e7fa08ae0c52fad84759288c92614fc1ecbc83ad
Johannes Hauschild <j.hauschild@tu-berlin.de>
2023-12-06 13:29:13 +0100

    Refactor to legacy citation

--- StructureAndAssumptions.thy
+++ StructureAndAssumptions.thy
@@ -8,14 +8,15 @@ locale world_dependant_kripke_structure =
   fixes
     \<comment>\<open>assigning a set of atomic propositions to each world.\<close>
     ap :: \<open>'i \<Rightarrow> 'ap set\<close> and
-    \<comment>\<open>$w1 \leq_w w2$ is enconding the notion "From $w_1$ $w_2$ is possible 
-       (as seen from $w$'s viewpoint).".\<close>
+    \<comment>\<open>$w1 \leq_w w2$ is enconding the notion "$w_1$ at least as similar to $w$ as is $w_2$".
+      A similar relation was defined by Lewis @{cite lewisCounterfactuals1973} as well as Finkbeiner 
+      and Siber cite\<open>finkbeinerCounterfactualsModuloTemporal2023\<close>\<close>
     accessibility :: \<open>'i \<Rightarrow> 'i \<Rightarrow> 'i \<Rightarrow> bool\<close> ("_ \<le><_> _" [70, 70, 70] 80)
   assumes
     reflexive [intro]: \<open>w1 \<le><w> w1\<close>
 
 text \<open>This structure is meant to implement a Kripke structure close to these employed by Baier and 
-      Katoen cite\<open>ctls_def\<close>. The name "accessibility" for the function encoding the similarity 
+      Katoen cite\<open>modelcheckingbaierkatoen\<close>. The name "accessibility" for the function encoding the similarity 
       between worlds is due to the fact, that it is reinterpreted to state the fact "$w_1$ is more 
       accessible from $w$ than $w_2$" in the following \texttt{locales}.
 
@@ -32,12 +33,6 @@ locale preordered_counterfactual_structure = world_dependant_kripke_structure ap
     \<comment>\<open>We assume, that any two worlds, which are comparable in respect to a world, are also 
       accessible from that world:\<close>
     meaningful_acessibility [intro]: \<open>\<lbrakk>w1 \<le><w> w2; w1 \<noteq> w2\<rbrakk> \<Longrightarrow> w \<le><w> w1 \<and> w \<le><w> w2\<close> 
-
-text \<open>Finkbeiner and Siber cite\<open>finkbeinerCounterfactualsModuloTemporal2023\<close>, 
-      as well as Lewis cite\<open>lewisCounterfactuals1973\<close>, require $\leq_w$ to be minimal,
-      meaning there exists no World $w'$ different from $w$ for which $w' \leq_w w$ holds.
-      Conducting the proofs, this assumption turned out to be spurious. 
-      Hence it is not included in any of the locales.\<close>
 
 locale finkbeiner_siber_structure = preordered_counterfactual_structure ap accessibility
   for
@@ -97,12 +92,6 @@ lemma (in preordered_counterfactual_structure) possible_phi:
   shows \<open>(\<exists>w1. w \<le><w> w1 \<and> w1 \<in> (\<phi> \<union> \<psi>) \<and>
   (\<exists>w2. w2 \<le><w> w1 \<and> w2 \<in> \<phi> \<and> (\<forall>w3. w3 \<le><w> w2 \<longrightarrow> w3 \<notin> \<psi>))) \<Longrightarrow> (\<exists>w1. w \<le><w> w1 \<and> w1 \<in> \<phi>)\<close> 
   by (metis meaningful_acessibility)
-
-(*Unnötig? 2023-11-23 
-lemma (in preordered_counterfactual_structure) subset_can_be_generalized:
-  shows \<open>(\<exists>w2. w2 \<le><w> w1 \<and> w2 \<in> \<phi> \<and> (\<forall>w3. w3 \<le><w> w2 \<longrightarrow> w3 \<notin> \<psi>)) \<Longrightarrow> 
-  (\<exists>w2. w2 \<le><w> w1 \<and> w2 \<in> (\<phi> \<union> \<psi>) \<and> (\<forall>w3. w3 \<le><w> w2 \<longrightarrow> w3 \<in> UNIV - (\<phi> \<union> \<psi>) \<union> (UNIV - \<psi>)))\<close>
-  by (metis Compl_eq_Diff_UNIV Compl_iff Un_iff)*)
 
 lemma (in preordered_counterfactual_structure) subset_generalized:
   shows\<open>(\<forall> w1. w \<le><w> w1 \<and> w1 \<in> \<psi> \<longrightarrow> (\<exists> w2. w2 \<le><w> w1 \<and> w2 \<in> \<phi> \<and> 
